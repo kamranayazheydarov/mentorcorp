302a223f4856c5a4040d940b520bbf8534227a77
kamranayazheydarov <k.a.heydarov@gmail.com>
2024-09-02 03:14:28 +0400

    files

--- main/settings.py
+++ main/settings.py
@@ -130,3 +130,6 @@ os.path.join(BASE_DIR, 'pages/templates/'),
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+import django_heroku
+django_heroku.settings(locals())
